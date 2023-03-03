f6ee412f3c80d981cdcda6b9adcbe622b3dbe020
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-03-03 12:43:46 +0900

    feat: Jpa 영속화시 날짜 자동 생성 entity 추가
    세부 사항:
    - 추상 엔티티로 단독으로 사용 불가
    - entity 생성시 createdAt, modifiedAt이 자동으로 업데이트 됨
    - entity 변경시 modifiedAt만 자동으로 변경됨

---
+++ src/main/java/com/almondia/meca/common/domain/entity/DateEntity.java
@@ -0,0 +1,29 @@
+package com.almondia.meca.common.domain.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SuperBuilder
+public abstract class DateEntity {
+
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime modifiedAt;
+}
