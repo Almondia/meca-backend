e458c82fdd134e1d772bcda0893c7d026344eefc
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-03-06 20:52:32 +0900

    feat: OAuth2 User 정보를 담을 attribute 추가
    세부 사항:
    - CustomOAuth2Client 에 OAuth api server에 유저 정보 요청 로직 추가

---
+++ src/main/java/com/almondia/meca/auth/oauth/infra/attribute/OAuth2UserAttribute.java
@@ -0,0 +1,21 @@
+package com.almondia.meca.auth.oauth.infra.attribute;
+
+import com.almondia.meca.member.domain.vo.OAuthType;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class OAuth2UserAttribute {
+
+	private final String name;
+	private final String email;
+	private final OAuthType oauthType;
+
+	public static OAuth2UserAttribute of(String name, String email, OAuthType oauthType) {
+		return new OAuth2UserAttribute(name, email, oauthType);
+	}
+}
