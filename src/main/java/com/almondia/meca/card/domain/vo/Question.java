04216a02ba1e361b17be63a43bd4ccbcaa1da32a
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-03-08 08:31:46 +0900

    feat: question vo 추가
    세부 사항
    - 500자 이하 글자만 입력 가능
    - 빈 공백 입력 불가능

---
+++ src/main/java/com/almondia/meca/card/domain/vo/Question.java
@@ -0,0 +1,32 @@
+package com.almondia.meca.card.domain.vo;
+
+import javax.persistence.Embeddable;
+
+import com.almondia.meca.common.configuration.jackson.module.wrapper.Wrapper;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Question implements Wrapper {
+
+	private static final int MAX_LENGTH = 500;
+	private String question;
+
+	public Question(String question) {
+		validateQuestion(question);
+		this.question = question;
+	}
+
+	private void validateQuestion(String question) {
+		if (question.isBlank()) {
+			throw new IllegalArgumentException("퀴즈 문제에 비우거나 공백만 입력해서는 안됩니다");
+		}
+		if (question.length() > MAX_LENGTH) {
+			throw new IllegalArgumentException(String.format("%d를 초과해서 입력하셨습니다", MAX_LENGTH));
+		}
+	}
+}
