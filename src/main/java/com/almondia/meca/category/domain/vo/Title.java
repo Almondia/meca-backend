d7f381b7afbc0b7ff034bcd9f3aedcb7d21b73c6
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-03-09 00:41:37 +0900

    feat: 카테고리 타이틀 vo 추가

---
+++ src/main/java/com/almondia/meca/category/domain/vo/Title.java
@@ -0,0 +1,38 @@
+package com.almondia.meca.category.domain.vo;
+
+import javax.persistence.Embeddable;
+
+import com.almondia.meca.common.configuration.jackson.module.wrapper.Wrapper;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Title implements Wrapper {
+
+	private static final int MAX_LENGTH = 20;
+
+	private String title;
+
+	public Title(String title) {
+		validateTitle(title);
+		this.title = title;
+	}
+
+	private void validateTitle(String title) {
+		if (title.length() > MAX_LENGTH) {
+			throw new IllegalArgumentException(String.format("%d를 초과해서 제목을 지을 수 없습니다", MAX_LENGTH));
+		}
+		if (title.isBlank()) {
+			throw new IllegalArgumentException("타이틀을 비우거나 공백만으로 초기화 할 수 없습니다");
+		}
+	}
+
+	@Override
+	public String toString() {
+		return title;
+	}
+}
