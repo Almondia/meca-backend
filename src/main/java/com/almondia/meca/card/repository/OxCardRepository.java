24c030b99725456b35202252eda397f919f24111
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-03-08 18:36:11 +0900

    feat: 각 암기 카드 종류별 repository 추가

---
+++ src/main/java/com/almondia/meca/card/repository/OxCardRepository.java
@@ -0,0 +1,9 @@
+package com.almondia.meca.card.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.almondia.meca.card.domain.entity.OxCard;
+import com.almondia.meca.common.domain.vo.Id;
+
+public interface OxCardRepository extends JpaRepository<OxCard, Id> {
+}
