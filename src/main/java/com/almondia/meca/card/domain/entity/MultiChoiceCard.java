0badc323accab253a81a5fb1baada151b9b492e7
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-03-09 00:19:59 +0900

    fix: 카드 getter시 null이 나오는 문제 수정

--- src/main/java/com/almondia/meca/card/domain/entity/MultiChoiceCard.java
+++ src/main/java/com/almondia/meca/card/domain/entity/MultiChoiceCard.java
@@ -5,6 +5,7 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 
+import com.almondia.meca.card.domain.vo.CardType;
 import com.almondia.meca.card.domain.vo.MultiChoiceAnswer;
 
 import lombok.AccessLevel;
@@ -25,4 +26,8 @@ public class MultiChoiceCard extends Card {
 	@Column(name = "multi_choice_answer", nullable = false)
 	private MultiChoiceAnswer multiChoiceAnswer;
 
+	@Override
+	public CardType getCardType() {
+		return CardType.MULTI_CHOICE;
+	}
 }
