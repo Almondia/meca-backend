59f3556a2f3fcf0fd3ee2298f30cce5096afca34
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-03-08 13:01:00 +0900

    feat: 이미지 타입 추가

---
+++ src/main/java/com/almondia/meca/card/domain/vo/Image.java
@@ -0,0 +1,35 @@
+package com.almondia.meca.card.domain.vo;
+
+import javax.persistence.Embeddable;
+
+import com.almondia.meca.common.configuration.jackson.module.wrapper.Wrapper;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Image implements Wrapper {
+
+	private static final int MAX_LINK_LENGTH = 255;
+
+	private String image;
+
+	public Image(String image) {
+		validateImages(image);
+		this.image = image;
+	}
+
+	private void validateImages(String image) {
+		if (image.length() > MAX_LINK_LENGTH) {
+			throw new IllegalArgumentException(String.format("image 링크 길이는 %d를 초과할 수 없습니다", MAX_LINK_LENGTH));
+		}
+	}
+
+	@Override
+	public String toString() {
+		return image;
+	}
+}
---
+++ src/test/java/com/almondia/meca/card/domain/vo/ImageTest.java
@@ -0,0 +1,18 @@
+package com.almondia.meca.card.domain.vo;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 255길이 초과한 링크를 넣을 수 없습니다
+ */
+class ImageTest {
+
+	@Test
+	@DisplayName("255길이 초과한 링크를 넣을 수 없습니다")
+	void validateLengthTest() {
+		assertThatThrownBy(() -> new Image("a".repeat(256))).isInstanceOf(IllegalArgumentException.class);
+	}
+}
