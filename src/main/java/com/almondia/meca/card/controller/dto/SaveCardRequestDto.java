5be47623d543a565b3e33849a26bf2dc21f39259
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-03-08 23:03:52 +0900

    feat: 카드 컨트롤러 추가
    세부 사항:
    - 카드 컨트롤러 테스트 결과 dto가 제대로 생성되지 않는 문제 수정

--- src/main/java/com/almondia/meca/card/controller/dto/SaveCardRequestDto.java
+++ src/main/java/com/almondia/meca/card/controller/dto/SaveCardRequestDto.java
@@ -8,21 +8,24 @@ import com.almondia.meca.card.domain.vo.Question;
 import com.almondia.meca.card.domain.vo.Title;
 import com.almondia.meca.common.domain.vo.Id;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SaveCardRequestDto {
 
-	private final Title title;
-	private final Question question;
-	private final Id categoryId;
-	private final String images;
-	private final CardType cardType;
-	private final OxAnswer oxAnswer;
-	private final KeywordAnswer keywordAnswer;
-	private final MultiChoiceAnswer multiChoiceAnswer;
+	private Title title;
+	private Question question;
+	private Id categoryId;
+	private String images;
+	private CardType cardType;
+	private OxAnswer oxAnswer;
+	private KeywordAnswer keywordAnswer;
+	private MultiChoiceAnswer multiChoiceAnswer;
 }
