e4bc8976a85bdebea8c2832fab97bf2b00e0d3a8
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-03-09 13:20:26 +0900

    test: WithMockMember 어노테이션과 관련 로직 추가
    세부 사항:
    - 해당 어노테이션으로 가상으로 인증 및 권한을 부여 가능

---
+++ src/test/java/com/almondia/meca/mock/security/WithMockMember.java
@@ -0,0 +1,31 @@
+package com.almondia.meca.mock.security;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import com.almondia.meca.member.domain.vo.OAuthType;
+import com.almondia.meca.member.domain.vo.Role;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@WithSecurityContext(factory = WithMockMemberSecurityContextFactory.class)
+public @interface WithMockMember {
+
+	String id() default "2825b9a9-d89a-4301-99b5-a7668a5b5fff";
+
+	String email() default "helloworld@naver.com";
+
+	String name() default "hello";
+
+	OAuthType oAuthType() default OAuthType.GOOGLE;
+
+	Role role() default Role.USER;
+}
