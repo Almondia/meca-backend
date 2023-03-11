b5d5d159871ba2a2cac13475a064e9d1b60a33ae
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-03-11 12:41:21 +0900

    feat: dto형태로 회원 profile 가져오는 service 로직 추가

---
+++ src/main/java/com/almondia/meca/member/controller/dto/MemberResponseDto.java
@@ -0,0 +1,28 @@
+package com.almondia.meca.member.controller.dto;
+
+import java.time.LocalDateTime;
+
+import com.almondia.meca.common.domain.vo.Id;
+import com.almondia.meca.member.domain.vo.Email;
+import com.almondia.meca.member.domain.vo.Name;
+import com.almondia.meca.member.domain.vo.OAuthType;
+import com.almondia.meca.member.domain.vo.Role;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@RequiredArgsConstructor
+@Getter
+public class MemberResponseDto {
+
+	private final Id memberId;
+	private final Name name;
+	private final Email email;
+	private final OAuthType oAuthType;
+	private final Role role;
+	private final boolean isDeleted;
+	private final LocalDateTime createdAt;
+	private final LocalDateTime modifiedAt;
+}
--- src/test/java/com/almondia/meca/member/service/MemberServiceTest.java
+++ src/test/java/com/almondia/meca/member/service/MemberServiceTest.java
@@ -2,6 +2,7 @@ package com.almondia.meca.member.service;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Import;
 
 import com.almondia.meca.auth.oauth.infra.attribute.OAuth2UserAttribute;
 import com.almondia.meca.common.domain.vo.Id;
+import com.almondia.meca.member.controller.dto.MemberResponseDto;
 import com.almondia.meca.member.domain.entity.Member;
 import com.almondia.meca.member.domain.vo.Email;
 import com.almondia.meca.member.domain.vo.Name;
@@ -24,6 +26,7 @@ import com.almondia.meca.member.repository.MemberRepository;
  * 1. saveOAuthAttribute 요청시 성공적으로 회원을 db에 저장해야한다.
  * 2. findMember 요청시 없다면 IllegalArgumentException 예외 출력
  * 3. findMember 요청시 있다면 Member 반환
+ * 4. findMyProfile 요청시 findMember와 로직은 동일하지만 Dto 형태로 반환
  */
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -71,5 +74,31 @@ class MemberServiceTest {
 			.hasFieldOrProperty("name")
 			.hasFieldOrProperty("role")
 			.hasFieldOrProperty("email");
+	}
+
+	@Test
+	@DisplayName("findMyProfile 요청시 findMember와 로직은 동일하지만 Dto 형태로 반환")
+	void shouldReturnMemberResponseDtoWHenCallFindMyProfileTest() {
+		Id id = Id.generateNextId();
+		Member member = Member.builder()
+			.memberId(id)
+			.oAuthType(OAuthType.KAKAO)
+			.name(new Name("hello"))
+			.email(new Email("hello@naver.com"))
+			.role(Role.USER)
+			.createdAt(LocalDateTime.now())
+			.modifiedAt(LocalDateTime.now())
+			.build();
+		memberRepository.save(member);
+		MemberResponseDto result = memberService.findMyProfile(id);
+		assertThat(result)
+			.hasFieldOrPropertyWithValue("memberId", member.getMemberId())
+			.hasFieldOrPropertyWithValue("name", member.getName())
+			.hasFieldOrPropertyWithValue("email", member.getEmail())
+			.hasFieldOrPropertyWithValue("oAuthType", member.getOAuthType())
+			.hasFieldOrPropertyWithValue("role", member.getRole())
+			.hasFieldOrPropertyWithValue("isDeleted", member.isDeleted())
+			.hasFieldOrProperty("createdAt")
+			.hasFieldOrProperty("modifiedAt");
 	}
 }
