71ff7ef4de349f7708cbe197d1b230b7ee697759
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-03-13 12:47:00 +0900

    feat: 카드 아이디와 외원 아이디를 동시에 만족하는 카드를 찾는 쿼리 로직 추가

--- src/main/java/com/almondia/meca/card/repository/KeywordCardRepository.java
+++ src/main/java/com/almondia/meca/card/repository/KeywordCardRepository.java
@@ -1,9 +1,12 @@
 package com.almondia.meca.card.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.almondia.meca.card.domain.entity.KeywordCard;
 import com.almondia.meca.common.domain.vo.Id;
 
 public interface KeywordCardRepository extends JpaRepository<KeywordCard, Id> {
+	Optional<KeywordCard> findByCardIdAndMemberId(Id cardId, Id memberId);
 }
--- src/main/java/com/almondia/meca/card/repository/OxCardRepository.java
+++ src/main/java/com/almondia/meca/card/repository/OxCardRepository.java
@@ -1,10 +1,13 @@
 package com.almondia.meca.card.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.almondia.meca.card.domain.entity.OxCard;
 import com.almondia.meca.common.domain.vo.Id;
 
 public interface OxCardRepository extends JpaRepository<OxCard, Id> {
+	Optional<OxCard> findByCardIdAndMemberId(Id cardId, Id memberId);
 
 }
