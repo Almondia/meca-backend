3c0dedfe542a004d817c73adb84e570fe66bf6e4
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-03-16 09:37:57 +0900

    feat: ulid로 생성된 데이터를 uuid로 변환
    세부 사항:
    - 생성 시점 기반 id로 바꾸면서 Id 대소 비교 기능 추가

--- src/main/java/com/almondia/meca/common/domain/vo/Id.java
+++ src/main/java/com/almondia/meca/common/domain/vo/Id.java
@@ -6,6 +6,8 @@ import java.util.UUID;
 import javax.persistence.Embeddable;
 
 import com.almondia.meca.common.configuration.jackson.module.wrapper.Wrapper;
+import com.github.f4b6a3.ulid.Ulid;
+import com.github.f4b6a3.ulid.UlidCreator;
 
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
@@ -14,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Embeddable
 @EqualsAndHashCode
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Id implements Serializable, Wrapper {
+public class Id implements Serializable, Wrapper, Comparable<Id> {
 
 	private static final long serialVersionUID = -2772995063676474658L;
 
@@ -29,12 +31,17 @@ public class Id implements Serializable, Wrapper {
 	}
 
 	public static Id generateNextId() {
-		UUID uuid = UUID.randomUUID();
-		return new Id(uuid);
+		Ulid ulid = UlidCreator.getMonotonicUlid();
+		return new Id(ulid.toUuid());
 	}
 
 	@Override
 	public String toString() {
 		return uuid.toString();
+	}
+
+	@Override
+	public int compareTo(Id o) {
+		return this.uuid.compareTo(o.uuid);
 	}
 }
