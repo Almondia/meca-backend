f778bf394e5e4725c5369bda238d967e8072a1ec
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-03-19 21:52:56 +0900

    feat: 카드 히스토리 등록 서비스 로직 추가

--- src/main/java/com/almondia/meca/cardhistory/controller/CardHistoryController.java
+++ src/main/java/com/almondia/meca/cardhistory/controller/CardHistoryController.java
@@ -3,6 +3,7 @@ package com.almondia.meca.cardhistory.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.almondia.meca.cardhistory.controller.dto.SaveRequestCardHistoryDto;
 import com.almondia.meca.cardhistory.service.CardHistoryService;
+import com.almondia.meca.member.domain.entity.Member;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,8 +24,10 @@ public class CardHistoryController {
 
 	@PostMapping("/simulation")
 	@Secured("ROLE_USER")
-	public ResponseEntity<String> saveHistories(@RequestBody SaveRequestCardHistoryDto saveRequestCardHistoryDto) {
-		cardHistoryService.saveHistories(saveRequestCardHistoryDto);
+	public ResponseEntity<String> saveHistories(
+		@AuthenticationPrincipal Member member,
+		@RequestBody SaveRequestCardHistoryDto saveRequestCardHistoryDto) {
+		cardHistoryService.saveHistories(saveRequestCardHistoryDto, member.getMemberId());
 		return ResponseEntity.status(HttpStatus.OK).build();
 	}
 }
--- src/main/java/com/almondia/meca/cardhistory/service/CardHistoryService.java
+++ src/main/java/com/almondia/meca/cardhistory/service/CardHistoryService.java
@@ -1,19 +1,20 @@
 package com.almondia.meca.cardhistory.service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.almondia.meca.card.domain.entity.Card;
-import com.almondia.meca.card.repository.CardRepository;
-import com.almondia.meca.card.sevice.checker.CardChecker;
+import com.almondia.meca.card.domain.repository.CardRepository;
 import com.almondia.meca.cardhistory.controller.dto.CardHistoryDto;
 import com.almondia.meca.cardhistory.controller.dto.SaveRequestCardHistoryDto;
 import com.almondia.meca.cardhistory.domain.entity.CardHistory;
 import com.almondia.meca.cardhistory.domain.repository.CardHistoryRepository;
-import com.almondia.meca.cardhistory.service.helper.CardHistoryFactory;
+import com.almondia.meca.common.domain.vo.Id;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,16 +24,39 @@ public class CardHistoryService {
 
 	private final CardHistoryRepository cardHistoryRepository;
 	private final CardRepository cardRepository;
-	private final CardChecker cardChecker;
 
 	@Transactional
-	public void saveHistories(SaveRequestCardHistoryDto saveRequestCardHistoryDto) {
+	public void saveHistories(SaveRequestCardHistoryDto saveRequestCardHistoryDto, Id memberId) {
+		Map<Id, List<Id>> categoryIdsByCardId = checkAuthority(saveRequestCardHistoryDto, memberId);
+		List<CardHistory> cardHistories = getCardHistories(saveRequestCardHistoryDto, categoryIdsByCardId);
+		cardHistoryRepository.saveAll(cardHistories);
+	}
+
+	private List<CardHistory> getCardHistories(SaveRequestCardHistoryDto saveRequestCardHistoryDto,
+		Map<Id, List<Id>> categoryIdsByCardId) {
 		List<CardHistory> cardHistories = new ArrayList<>();
 		for (CardHistoryDto cardHistoryDto : saveRequestCardHistoryDto.getCardHistoryDtos()) {
-			Card card = cardRepository.findById(cardHistoryDto.getCardId())
-				.orElseThrow(() -> new IllegalArgumentException("해당 카드가 존재하지 않습니다"));
-			cardHistories.add(CardHistoryFactory.makeCardHistory(cardHistoryDto, card.getCategoryId()));
+			CardHistory cardHistory = CardHistory.builder()
+				.cardHistoryId(Id.generateNextId())
+				.cardId(cardHistoryDto.getCardId())
+				.categoryId(categoryIdsByCardId.get(cardHistoryDto.getCardId()).get(0))
+				.userAnswer(cardHistoryDto.getUserAnswer())
+				.score(cardHistoryDto.getScore())
+				.build();
+			cardHistories.add(cardHistory);
 		}
-		cardHistoryRepository.saveAll(cardHistories);
+		return cardHistories;
+	}
+
+	private Map<Id, List<Id>> checkAuthority(SaveRequestCardHistoryDto saveRequestCardHistoryDto, Id memberId) {
+		List<Id> cardIds = saveRequestCardHistoryDto.getCardHistoryDtos().stream()
+			.map(CardHistoryDto::getCardId)
+			.collect(Collectors.toList());
+		Map<Id, List<Id>> categoryIdsByCardId = cardRepository.findMapByListOfCardIdAndMemberId(cardIds, memberId);
+		long allValuesCount = categoryIdsByCardId.values().stream().mapToLong(Collection::size).sum();
+		if (allValuesCount != cardIds.size()) {
+			throw new IllegalArgumentException("권한이 없거나 옳바르지 않은 입력을 하셨습니다");
+		}
+		return categoryIdsByCardId;
 	}
 }
