f06fd476145ba686855e4cbe8b09eef7b30ce5d6
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-03-19 10:44:01 +0900

    feat: 카드 히스토리 엔티티 추가

---
+++ src/main/java/com/almondia/meca/cardhistory/domain/entity/CardHistory.java
@@ -0,0 +1,57 @@
+package com.almondia.meca.cardhistory.domain.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.almondia.meca.cardhistory.domain.vo.Answer;
+import com.almondia.meca.cardhistory.domain.vo.Score;
+import com.almondia.meca.common.domain.vo.Id;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "card_history")
+@Getter
+public class CardHistory {
+
+	@EmbeddedId
+	@AttributeOverride(name = "uuid", column = @Column(name = "card_history_id", nullable = false, columnDefinition = "BINARY(16)"))
+	Id cardHistoryId;
+
+	@Embedded
+	@AttributeOverride(name = "uuid", column = @Column(name = "card_id", nullable = false, columnDefinition = "BINARY(16)"))
+	Id cardId;
+
+	@Embedded
+	@AttributeOverride(name = "uuid", column = @Column(name = "category_id", nullable = false, columnDefinition = "BINARY(16)"))
+	Id categoryId;
+
+	@Embedded
+	@AttributeOverride(name = "answer", column = @Column(name = "user_answer", nullable = false, length = 25))
+	Answer userAnswer;
+
+	@Embedded
+	@AttributeOverride(name = "score", column = @Column(name = "score", nullable = false))
+	Score score;
+
+	@CreatedDate
+	LocalDateTime createdAt;
+}
