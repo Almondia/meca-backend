b3b9ebe3c899decefc921790f6e413e0f38f99d7
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-03-20 09:44:28 +0900

    feat: 카드 삭제 API 추가

--- src/main/java/com/almondia/meca/card/controller/CardController.java
+++ src/main/java/com/almondia/meca/card/controller/CardController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,6 +69,16 @@ public class CardController {
 		CursorPage<CardResponseDto> responseDto = cardService.searchCursorPagingCard(pageSize, categoryId, criteria,
 			SortOption.of(CardSortField.CARD_ID, sortOrder), member.getMemberId());
 		return ResponseEntity.ok(responseDto);
+	}
+
+	@Secured("ROLE_USER")
+	@DeleteMapping("/{cardId}")
+	public ResponseEntity<String> deleteCard(
+		@AuthenticationPrincipal Member member,
+		@PathVariable(value = "cardId") Id cardId
+	) {
+		cardService.deleteCard(cardId, member.getMemberId());
+		return ResponseEntity.status(HttpStatus.OK).body("");
 	}
 
 	private CardSearchCriteria makeCursorCriteria(Id categoryId, Id lastId,
