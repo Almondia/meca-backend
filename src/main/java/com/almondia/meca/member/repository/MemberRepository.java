e25fa3bf96d02099ba99a2dbecfcb1b4b6848696
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-03-22 19:16:42 +0900

    feat: member에 oAuthId를 통해 회원는 찾는 쿼리 추가

--- src/main/java/com/almondia/meca/member/repository/MemberRepository.java
+++ src/main/java/com/almondia/meca/member/repository/MemberRepository.java
@@ -1,9 +1,12 @@
 package com.almondia.meca.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.almondia.meca.common.domain.vo.Id;
 import com.almondia.meca.member.domain.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Id> {
+	Optional<Member> findByOauthId(String oAuthId);
 }
