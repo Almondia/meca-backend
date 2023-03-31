bd978abdf373c5f682d72c07a7f007ca483ff404
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-03-31 15:34:36 +0900

    feat: S3 presignedurl 요청 컴포넌트 추가

---
+++ src/main/java/com/almondia/meca/common/infra/s3/S3PreSignedUrlRequest.java
@@ -0,0 +1,29 @@
+package com.almondia.meca.common.infra.s3;
+
+import java.net.URL;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import com.almondia.meca.common.configuration.s3.S3Properties;
+import com.amazonaws.HttpMethod;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class S3PreSignedUrlRequest {
+
+	private final AmazonS3Client s3Client;
+	private final S3Properties s3Properties;
+
+	public URL requestPostPreSignedUrl(String objectKey, Date expiration) {
+		GeneratePresignedUrlRequest generatePresignedUrlRequest =
+			new GeneratePresignedUrlRequest(s3Properties.getBucket(), objectKey)
+				.withMethod(HttpMethod.POST)
+				.withExpiration(expiration);
+		return s3Client.generatePresignedUrl(generatePresignedUrlRequest);
+	}
+}
