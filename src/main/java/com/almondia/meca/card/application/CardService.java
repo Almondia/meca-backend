82861e1d4060b2e1e517b9b345fdd726c1c39dfd
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-04-12 16:20:33 +0900

    fix: 없을시 null로 인한 응답 에러 수정

--- src/main/java/com/almondia/meca/card/application/CardService.java
+++ src/main/java/com/almondia/meca/card/application/CardService.java
@@ -89,7 +89,8 @@ public class CardService {
 
 	@Transactional(readOnly = true)
 	public SharedCardResponseDto findSharedCard(Id cardId) {
-		return cardRepository.findSharedCard(cardId);
+		return cardRepository.findSharedCard(cardId)
+			.orElseThrow(() -> new IllegalArgumentException("공유된 카테고리의 카드가 존재하지 않습니다"));
 	}
 
 	@Transactional(readOnly = true)
