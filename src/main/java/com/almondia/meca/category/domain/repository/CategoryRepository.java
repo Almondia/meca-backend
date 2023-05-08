6a3b5ee38804068d1782fd7469cd132dc93d53c0
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-05-08 10:02:33 +0900

    feat: 카테고리id 와 isDeleted 속성을 이용해 인스턴스를 찾는 쿼리 추가

--- src/main/java/com/almondia/meca/category/domain/repository/CategoryRepository.java
+++ src/main/java/com/almondia/meca/category/domain/repository/CategoryRepository.java
@@ -11,4 +11,6 @@ import com.almondia.meca.common.domain.vo.Id;
 public interface CategoryRepository extends JpaRepository<Category, Id>, CategoryQueryDslRepository {
 	Optional<Category> findByCategoryIdAndMemberId(Id categoryId, Id memberId);
 
+	Optional<Category> findByCategoryIdAndIsDeleted(Id categoryId, boolean isDeleted);
+
 }
