8efda82e208950be06917c11f2f10a44a84f47c3
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-05-14 18:52:18 +0900

    test: 카드 히스토리 응답 dto 생성 메서드 추가

--- src/test/java/com/almondia/meca/helper/CardHistoryTestHelper.java
+++ src/test/java/com/almondia/meca/helper/CardHistoryTestHelper.java
@@ -3,10 +3,12 @@ package com.almondia.meca.helper;
 import java.time.LocalDateTime;
 import java.util.Random;
 
+import com.almondia.meca.cardhistory.controller.dto.CardHistoryResponseDto;
 import com.almondia.meca.cardhistory.domain.entity.CardHistory;
 import com.almondia.meca.cardhistory.domain.vo.Answer;
 import com.almondia.meca.cardhistory.domain.vo.Score;
 import com.almondia.meca.common.domain.vo.Id;
+import com.almondia.meca.member.domain.vo.Name;
 
 public class CardHistoryTestHelper {
 
@@ -32,6 +34,19 @@ public class CardHistoryTestHelper {
 			.solvedUserId(solvedUserId)
 			.userAnswer(new Answer("answer"))
 			.score(new Score(random.nextInt(100)))
+			.createdAt(LocalDateTime.now())
+			.build();
+	}
+
+	public static CardHistoryResponseDto generateCardHistoryResponseDto() {
+		return CardHistoryResponseDto.builder()
+			.cardHistoryId(Id.generateNextId())
+			.solvedUserId(Id.generateNextId())
+			.solvedUserName(new Name("name"))
+			.userAnswer(new Answer("answer"))
+			.score(new Score(random.nextInt(100)))
+			.categoryId(Id.generateNextId())
+			.cardId(Id.generateNextId())
 			.createdAt(LocalDateTime.now())
 			.build();
 	}
