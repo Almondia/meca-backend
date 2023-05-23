bd467ce047b5baaf1c506d0e7c724c86c889fc74
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-05-23 13:50:17 +0900

    feat: 삭제되지 않은 카테고리 추천 인스턴 조회 쿼리 추가

---
+++ src/main/java/com/almondia/meca/recommand/domain/repository/CategoryRecommendRepository.java
@@ -0,0 +1,14 @@
+package com.almondia.meca.recommand.domain.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.almondia.meca.common.domain.vo.Id;
+import com.almondia.meca.recommand.domain.entity.CategoryRecommend;
+
+public interface CategoryRecommendRepository extends JpaRepository<CategoryRecommend, Id> {
+	Optional<CategoryRecommend> findByCategoryIdAndRecommendMemberIdAndIsDeletedFalse(Id categoryId,
+		Id recommendMemberId);
+
+}
