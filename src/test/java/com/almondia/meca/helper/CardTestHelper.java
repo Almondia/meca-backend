e2d9279b71d491e4370fcfc9eec6c23d9badbb49
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-06-05 17:08:01 +0900

    test: card type별 테스트용 정적 팩토리 메서드 추가

--- src/test/java/com/almondia/meca/helper/CardTestHelper.java
+++ src/test/java/com/almondia/meca/helper/CardTestHelper.java
@@ -2,9 +2,13 @@ package com.almondia.meca.helper;
 
 import java.time.LocalDateTime;
 
+import com.almondia.meca.card.domain.entity.KeywordCard;
+import com.almondia.meca.card.domain.entity.MultiChoiceCard;
 import com.almondia.meca.card.domain.entity.OxCard;
 import com.almondia.meca.card.domain.vo.CardType;
 import com.almondia.meca.card.domain.vo.Description;
+import com.almondia.meca.card.domain.vo.KeywordAnswer;
+import com.almondia.meca.card.domain.vo.MultiChoiceAnswer;
 import com.almondia.meca.card.domain.vo.OxAnswer;
 import com.almondia.meca.card.domain.vo.Question;
 import com.almondia.meca.card.domain.vo.Title;
@@ -41,6 +45,34 @@ public class CardTestHelper {
 			.createdAt(LocalDateTime.now())
 			.modifiedAt(LocalDateTime.now())
 			.isDeleted(true)
+			.build();
+	}
+
+	public static KeywordCard genKeywordCard(Id memberId, Id categoryId, Id cardId) {
+		return KeywordCard.builder()
+			.cardId(cardId)
+			.categoryId(categoryId)
+			.memberId(memberId)
+			.title(new Title("title"))
+			.description(new Description("description"))
+			.question(new Question("question"))
+			.keywordAnswer(KeywordAnswer.valueOf("keyword"))
+			.cardType(CardType.KEYWORD)
+			.isDeleted(false)
+			.build();
+	}
+
+	public static MultiChoiceCard genMultiChoiceCard(Id memberId, Id categoryId, Id cardId) {
+		return MultiChoiceCard.builder()
+			.cardId(cardId)
+			.categoryId(categoryId)
+			.memberId(memberId)
+			.title(new Title("title"))
+			.description(new Description("description"))
+			.question(new Question("question"))
+			.multiChoiceAnswer(MultiChoiceAnswer.valueOf("1"))
+			.cardType(CardType.MULTI_CHOICE)
+			.isDeleted(false)
 			.build();
 	}
 }
