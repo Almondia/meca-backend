37bcd9a75368dbcd22531faf3790d443543237ca
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-06-08 20:36:49 +0900

    feat: cardSnapShot JPA 세부 속성 지정

--- src/main/java/com/almondia/meca/cardhistory/domain/vo/CardSnapShot.java
+++ src/main/java/com/almondia/meca/cardhistory/domain/vo/CardSnapShot.java
@@ -2,8 +2,13 @@ package com.almondia.meca.cardhistory.domain.vo;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
+import com.almondia.meca.card.domain.entity.Card;
 import com.almondia.meca.card.domain.vo.CardType;
 import com.almondia.meca.card.domain.vo.Description;
 import com.almondia.meca.card.domain.vo.Question;
@@ -24,12 +29,25 @@ import lombok.ToString;
 @ToString
 public final class CardSnapShot {
 
+	@AttributeOverride(name = "title", column = @Column(name = "card_title", nullable = false, length = 120))
 	private Title title;
+
+	@AttributeOverride(name = "question", column = @Column(name = "card_question", nullable = false, length = 500))
 	private Question question;
+
+	@AttributeOverride(name = "answer", column = @Column(name = "card_answer", nullable = false))
 	private String answer;
+
+	@Enumerated(EnumType.STRING)
 	private CardType cardType;
+
+	@AttributeOverride(name = "description", column = @Column(name = "card_description", nullable = false, length = 2_1000, columnDefinition = "TEXT"))
 	private Description description;
+
+	@Column(name = "card_created_at", nullable = false, updatable = false)
 	private LocalDateTime createdAt;
+
+	@Column(name = "card_modified_at", nullable = false)
 	private LocalDateTime modifiedAt;
 
 	@Builder
@@ -53,5 +71,17 @@ public final class CardSnapShot {
 		this.description = description;
 		this.createdAt = createdAt;
 		this.modifiedAt = modifiedAt;
+	}
+
+	public static CardSnapShot copyShot(Card card) {
+		return CardSnapShot.builder()
+			.title(card.getTitle())
+			.question(card.getQuestion())
+			.answer(card.getAnswer())
+			.cardType(card.getCardType())
+			.description(card.getDescription())
+			.createdAt(card.getCreatedAt())
+			.modifiedAt(card.getModifiedAt())
+			.build();
 	}
 }
