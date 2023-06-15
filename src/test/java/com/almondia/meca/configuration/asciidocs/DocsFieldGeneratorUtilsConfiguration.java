a941f0d3409c25953b5ed73392ca607cc162ba02
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-06-15 21:30:12 +0900

    test: 테스트 전용 configuration 추가

---
+++ src/test/java/com/almondia/meca/configuration/asciidocs/DocsFieldGeneratorUtilsConfiguration.java
@@ -0,0 +1,38 @@
+package com.almondia.meca.configuration.asciidocs;
+
+import java.time.LocalDateTime;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import com.almondia.meca.asciidocs.fields.DocsFieldGeneratorUtils;
+import com.almondia.meca.asciidocs.fields.reflection.CommonTypeCheckerManager;
+import com.almondia.meca.asciidocs.fields.reflection.CommonTypeCheckerManagerImpl;
+import com.almondia.meca.asciidocs.fields.reflection.FieldVisitor;
+import com.almondia.meca.asciidocs.fields.reflection.FieldVisitorImpl;
+import com.almondia.meca.common.configuration.jackson.module.wrapper.Wrapper;
+
+@TestConfiguration
+public class DocsFieldGeneratorUtilsConfiguration {
+
+	@Bean
+	public DocsFieldGeneratorUtils docsFieldGeneratorUtils() {
+		CommonTypeCheckerManager commonTypeCheckerManager = new CommonTypeCheckerManagerImpl();
+		commonTypeCheckerManager.addChecker(type -> {
+			if (type instanceof Class) {
+				Class<?> classType = (Class<?>)type;
+				return Wrapper.class.isAssignableFrom(classType);
+			}
+			return false;
+		});
+		commonTypeCheckerManager.addChecker(type -> {
+			if (type instanceof Class) {
+				Class<?> classType = (Class<?>)type;
+				return LocalDateTime.class.isAssignableFrom(classType);
+			}
+			return false;
+		});
+		FieldVisitor fieldVisitor = new FieldVisitorImpl(commonTypeCheckerManager);
+		return new DocsFieldGeneratorUtils(fieldVisitor);
+	}
+}
