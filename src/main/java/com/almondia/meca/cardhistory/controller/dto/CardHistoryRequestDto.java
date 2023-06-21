db61b1a268e190ad7032f6e68ca1432dfc71906c
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-06-21 16:09:40 +0900

    fix: Score 속성 삭제

--- src/main/java/com/almondia/meca/cardhistory/controller/dto/CardHistoryRequestDto.java
+++ src/main/java/com/almondia/meca/cardhistory/controller/dto/CardHistoryRequestDto.java
@@ -1,7 +1,6 @@
 package com.almondia.meca.cardhistory.controller.dto;
 
 import com.almondia.meca.cardhistory.domain.vo.Answer;
-import com.almondia.meca.cardhistory.domain.vo.Score;
 import com.almondia.meca.common.domain.vo.Id;
 
 import lombok.AccessLevel;
@@ -18,5 +17,4 @@ public class CardHistoryRequestDto {
 
 	private Id cardId;
 	private Answer userAnswer;
-	private Score score;
 }
