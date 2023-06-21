5699b6f5ebbc25a633cde35f3260f40367cf7607
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-06-21 16:09:23 +0900

    fix: CardHistoryFactory중 단일 생성 메서드 외에 모두 삭제

--- src/test/java/com/almondia/meca/cardhistory/application/helper/CardHistoryFactoryTest.java
+++ src/test/java/com/almondia/meca/cardhistory/application/helper/CardHistoryFactoryTest.java
@@ -2,16 +2,15 @@ package com.almondia.meca.cardhistory.application.helper;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.List;
-
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import com.almondia.meca.card.domain.entity.Card;
 import com.almondia.meca.cardhistory.controller.dto.CardHistoryRequestDto;
-import com.almondia.meca.cardhistory.controller.dto.SaveRequestCardHistoryDto;
 import com.almondia.meca.cardhistory.domain.entity.CardHistory;
 import com.almondia.meca.cardhistory.domain.vo.Answer;
+import com.almondia.meca.cardhistory.domain.vo.CardSnapShot;
 import com.almondia.meca.cardhistory.domain.vo.Score;
 import com.almondia.meca.common.domain.vo.Id;
 import com.almondia.meca.helper.CardTestHelper;
@@ -21,35 +20,33 @@ import com.almondia.meca.helper.CardTestHelper;
  */
 class CardHistoryFactoryTest {
 
-	@Test
-	@DisplayName("dto에서 정상정인 인스턴스 생성하는지 테스트")
-	void shouldGenerateNewInstanceFromDtoTest() {
-		Id memberId = Id.generateNextId();
-		Id categoryId = Id.generateNextId();
-		Id cardId = Id.generateNextId();
-		Id solvedMemberId = Id.generateNextId();
+	@Nested
+	@DisplayName("makeCardHistory 테스트")
+	class MakeCardHistoryTest {
 
-		CardHistoryRequestDto cardHistoryRequestDto = CardHistoryRequestDto.builder()
-			.cardId(cardId)
-			.score(new Score(100))
-			.userAnswer(new Answer("123"))
-			.build();
+		@Test
+		@DisplayName("request와 회원 아이디 그리고 score를 활용해 cardHistory를 생성해야 함")
+		void shouldReturnCardHistoryUsingArguments() {
+			// given
+			final Id cardId = Id.generateNextId();
+			final Id solvedMemberId = Id.generateNextId();
+			final Answer answer = new Answer("O");
+			final Score score = new Score(100);
+			CardHistoryRequestDto cardHistoryRequestDto = new CardHistoryRequestDto(cardId, answer);
+			Card card = CardTestHelper.genOxCard(solvedMemberId, Id.generateNextId(), cardId);
 
-		Card card = CardTestHelper.genOxCard(memberId, categoryId, cardId);
-		SaveRequestCardHistoryDto saveRequestCardHistoryDto = SaveRequestCardHistoryDto.builder()
-			.cardHistories(List.of(cardHistoryRequestDto))
-			.build();
+			// when
+			CardHistory cardHistory = CardHistoryFactory.makeCardHistory(cardHistoryRequestDto, card, solvedMemberId,
+				score);
 
-		List<CardHistory> result = CardHistoryFactory.makeCardHistories(saveRequestCardHistoryDto, List.of(card),
-			solvedMemberId);
-		assertThat(result.get(0))
-			.hasFieldOrProperty("cardHistoryId")
-			.hasFieldOrPropertyWithValue("cardId", cardHistoryRequestDto.getCardId())
-			.hasFieldOrPropertyWithValue("score", cardHistoryRequestDto.getScore())
-			.hasFieldOrPropertyWithValue("userAnswer", cardHistoryRequestDto.getUserAnswer())
-			.hasFieldOrPropertyWithValue("solvedMemberId", solvedMemberId)
-			.hasFieldOrPropertyWithValue("isDeleted", false)
-			.hasFieldOrProperty("cardSnapShot");
-
+			// then
+			assertThat(cardHistory)
+				.hasFieldOrProperty("cardHistoryId")
+				.hasFieldOrPropertyWithValue("cardId", cardId)
+				.hasFieldOrPropertyWithValue("solvedMemberId", solvedMemberId)
+				.hasFieldOrPropertyWithValue("userAnswer", answer)
+				.hasFieldOrPropertyWithValue("score", score)
+				.hasFieldOrPropertyWithValue("cardSnapShot", CardSnapShot.copyShot(card));
+		}
 	}
 }
