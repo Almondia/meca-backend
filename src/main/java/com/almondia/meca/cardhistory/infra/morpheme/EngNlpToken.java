b4299cc3dbd36028c5efc6083d9f3454d2d9dcaf
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-06-22 13:30:17 +0900

    feat: EngNlpToken 추가

---
+++ src/main/java/com/almondia/meca/cardhistory/infra/morpheme/EngNlpToken.java
@@ -0,0 +1,16 @@
+package com.almondia.meca.cardhistory.infra.morpheme;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class EngNlpToken {
+
+	private final String morph;
+	private final String pos;
+}
