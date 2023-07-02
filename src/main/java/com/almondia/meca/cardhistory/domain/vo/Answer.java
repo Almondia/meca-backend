3f9323d0a5775c4915578d92720d8c999efe3f8f
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-07-02 23:50:59 +0900

    refactor: 카드 히스토리 Answer 최대길이 500으로 수정

--- src/main/java/com/almondia/meca/cardhistory/domain/vo/Answer.java
+++ src/main/java/com/almondia/meca/cardhistory/domain/vo/Answer.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Answer implements Wrapper {
 
-	private static final int MAX_LENGTH = 255;
+	private static final int MAX_LENGTH = 500;
 
 	private String answer;
 
