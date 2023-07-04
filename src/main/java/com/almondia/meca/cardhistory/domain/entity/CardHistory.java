02118f30a9c42d75ebaa170c518e494945cb27b0
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-07-04 13:23:15 +0900

    refactor: 카드 히스토리 엔티티 속성 수정
    세부 사항:
    - id의 columnDefinition 제거

--- src/main/java/com/almondia/meca/cardhistory/domain/entity/CardHistory.java
+++ src/main/java/com/almondia/meca/cardhistory/domain/entity/CardHistory.java
@@ -34,15 +34,15 @@ import lombok.NoArgsConstructor;
 public class CardHistory {
 
 	@EmbeddedId
-	@AttributeOverride(name = "uuid", column = @Column(name = "card_history_id", nullable = false, columnDefinition = "BINARY(16)"))
+	@AttributeOverride(name = "uuid", column = @Column(name = "card_history_id", nullable = false, length = 16))
 	private Id cardHistoryId;
 
 	@Embedded
-	@AttributeOverride(name = "uuid", column = @Column(name = "solved_user_id", nullable = false, columnDefinition = "BINARY(16)"))
+	@AttributeOverride(name = "uuid", column = @Column(name = "solved_user_id", nullable = false, length = 16))
 	private Id solvedMemberId;
 
 	@Embedded
-	@AttributeOverride(name = "uuid", column = @Column(name = "card_id", nullable = false, columnDefinition = "BINARY(16)"))
+	@AttributeOverride(name = "uuid", column = @Column(name = "card_id", nullable = false, length = 16))
 	private Id cardId;
 
 	@Embedded
