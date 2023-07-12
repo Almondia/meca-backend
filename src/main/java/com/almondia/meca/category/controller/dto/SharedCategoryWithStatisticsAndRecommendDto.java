67dfb5a0e469ff977490c51dd7852eb04885faa2
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-07-12 20:12:19 +0900

    feat: SharedCategoryWithStatisticsAndRecommendDto 추가

---
+++ src/main/java/com/almondia/meca/category/controller/dto/SharedCategoryWithStatisticsAndRecommendDto.java
@@ -0,0 +1,23 @@
+package com.almondia.meca.category.controller.dto;
+
+import com.almondia.meca.member.application.helper.MemberMapper;
+import com.almondia.meca.member.controller.dto.MemberDto;
+import com.almondia.meca.member.domain.entity.Member;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@RequiredArgsConstructor
+@ToString
+public class SharedCategoryWithStatisticsAndRecommendDto {
+
+	private final CategoryWithHistoryResponseDto categoryDto;
+	private final MemberDto memberDto;
+
+	public SharedCategoryWithStatisticsAndRecommendDto(CategoryWithHistoryResponseDto categoryDto, Member member) {
+		this.categoryDto = categoryDto;
+		this.memberDto = MemberMapper.fromEntityToDto(member);
+	}
+}
