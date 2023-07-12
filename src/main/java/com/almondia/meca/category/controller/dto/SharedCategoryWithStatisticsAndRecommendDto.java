606bed5faa1d6c623ea9033c151dd78282e2ade0
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-07-12 21:07:20 +0900

    fix: 내부 구조 수정 및 CategoryWithStatisticsDto 상속
    세부 사항:
    - CategoryWithStatisticsDto의 구현체로 수정
    - 내부 구조 단순화

--- src/main/java/com/almondia/meca/category/controller/dto/SharedCategoryWithStatisticsAndRecommendDto.java
+++ src/main/java/com/almondia/meca/category/controller/dto/SharedCategoryWithStatisticsAndRecommendDto.java
@@ -1,5 +1,7 @@
 package com.almondia.meca.category.controller.dto;
 
+import com.almondia.meca.category.application.helper.CategoryMapper;
+import com.almondia.meca.category.domain.entity.Category;
 import com.almondia.meca.member.application.helper.MemberMapper;
 import com.almondia.meca.member.controller.dto.MemberDto;
 import com.almondia.meca.member.domain.entity.Member;
@@ -11,13 +13,18 @@ import lombok.ToString;
 @Getter
 @RequiredArgsConstructor
 @ToString
-public class SharedCategoryWithStatisticsAndRecommendDto {
+public class SharedCategoryWithStatisticsAndRecommendDto implements CategoryWithStatisticsDto {
 
-	private final CategoryWithStatisticsResponseDto categoryDto;
-	private final MemberDto memberDto;
+	private final CategoryDto category;
+	private final StatisticsDto statistics;
+	private final MemberDto member;
+	private final long likeCount;
 
-	public SharedCategoryWithStatisticsAndRecommendDto(CategoryWithStatisticsResponseDto categoryDto, Member member) {
-		this.categoryDto = categoryDto;
-		this.memberDto = MemberMapper.fromEntityToDto(member);
+	public SharedCategoryWithStatisticsAndRecommendDto(Category category, Member member, StatisticsDto statisticsDto,
+		long likeCount) {
+		this.category = CategoryMapper.entityToCategoryDto(category);
+		this.member = MemberMapper.fromEntityToDto(member);
+		this.statistics = statisticsDto;
+		this.likeCount = likeCount;
 	}
 }
