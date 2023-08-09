e1fcaa1a8f1490a5f68731eb70a8ac08622bc9a7
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-08-09 16:52:33 +0900

    refactor: 카테고리 API 회원 응답 간소화

--- src/main/java/com/almondia/meca/category/controller/dto/SharedCategoryResponseDto.java
+++ src/main/java/com/almondia/meca/category/controller/dto/SharedCategoryResponseDto.java
@@ -2,8 +2,6 @@ package com.almondia.meca.category.controller.dto;
 
 import com.almondia.meca.category.application.helper.CategoryMapper;
 import com.almondia.meca.category.domain.entity.Category;
-import com.almondia.meca.member.application.helper.MemberMapper;
-import com.almondia.meca.member.controller.dto.MemberDto;
 import com.almondia.meca.member.domain.entity.Member;
 
 import lombok.Getter;
@@ -14,21 +12,29 @@ import lombok.ToString;
 public class SharedCategoryResponseDto {
 
 	private final CategoryDto category;
-	private final MemberDto member;
+	private final CategoryMemberDto member;
 	private long likeCount;
 
 	public SharedCategoryResponseDto(Category category, Member member, long likeCount) {
 		this.category = CategoryMapper.entityToCategoryDto(category);
-		this.member = MemberMapper.fromEntityToDto(member);
+		this.member = toCategoryMemberDto(member);
 		this.likeCount = likeCount;
 	}
 
 	public SharedCategoryResponseDto(Category category, Member member) {
 		this.category = CategoryMapper.entityToCategoryDto(category);
-		this.member = MemberMapper.fromEntityToDto(member);
+		this.member = toCategoryMemberDto(member);
 	}
 
 	public void setLikeCount(long likeCount) {
 		this.likeCount = likeCount;
+	}
+
+	private CategoryMemberDto toCategoryMemberDto(Member member) {
+		return CategoryMemberDto.builder()
+			.memberId(member.getMemberId())
+			.name(member.getName())
+			.profile(member.getProfile())
+			.build();
 	}
 }
