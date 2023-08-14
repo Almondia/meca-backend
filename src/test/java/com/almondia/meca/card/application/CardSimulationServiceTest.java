8fdd055895e4d547bfb79c7c343e134ea4daad67
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-08-14 14:22:04 +0900

    test: simulationScore 스텁 제거

--- src/test/java/com/almondia/meca/card/application/CardSimulationServiceTest.java
+++ src/test/java/com/almondia/meca/card/application/CardSimulationServiceTest.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 
 import javax.persistence.EntityManager;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -23,6 +22,7 @@ import org.springframework.security.access.AccessDeniedException;
 
 import com.almondia.meca.card.controller.dto.CardCountGroupByScoreDto;
 import com.almondia.meca.card.controller.dto.CardDto;
+import com.almondia.meca.card.domain.entity.Card;
 import com.almondia.meca.card.domain.repository.CardRepository;
 import com.almondia.meca.cardhistory.domain.repository.CardHistoryRepository;
 import com.almondia.meca.category.domain.entity.Category;
@@ -168,56 +168,49 @@ class CardSimulationServiceTest {
 		@DisplayName("삭제된 카테고리를 조회한 경우")
 		void shouldThrowIllegalArgumentExceptionWhenCategoryIsDeletedTest() {
 			// given
-			Mockito.doReturn(Optional.empty())
-				.when(categoryRepository)
-				.findByCategoryIdAndIsDeleted(any(), anyBoolean());
-			Mockito.doReturn(
-					List.of(CardTestHelper.genOxCard(Id.generateNextId(), Id.generateNextId(), Id.generateNextId())))
-				.when(cardRepository)
-				.findByCategoryIdAndIsDeleted(any(), anyBoolean());
-			Id categoryId = Id.generateNextId();
 			Id memberId = Id.generateNextId();
+			Id categoryId = Id.generateNextId();
+			Category category = CategoryTestHelper.generateUnSharedCategory("title", memberId,
+				categoryId);
+			category.delete();
+			em.persist(category);
 
 			// expect
 			assertThatThrownBy(
-				() -> cardSimulationService.simulateScore(categoryId, memberId, 100)).isInstanceOf(
-				IllegalArgumentException.class);
+				() -> cardSimulationService.simulateScore(categoryId, memberId, 100))
+				.isInstanceOf(IllegalArgumentException.class);
+
 		}
 
 		@Test
 		@DisplayName("본인 카테고리가 아니거나 남의 카테고리지만 공유가 되지 않은 경우 접근 불가 예외를 발생한다")
 		void shouldThrowAccessDeniedExceptionWhenNotMyCategoryOrNotSharedTest() {
 			// given
-			Mockito.doReturn(Optional.of(
-					CategoryTestHelper.generateUnSharedCategory("title", Id.generateNextId(), Id.generateNextId())))
-				.when(categoryRepository)
-				.findByCategoryIdAndIsDeleted(any(), anyBoolean());
-			Mockito.doReturn(
-					List.of(CardTestHelper.genOxCard(Id.generateNextId(), Id.generateNextId(), Id.generateNextId())))
-				.when(cardRepository)
-				.findCardByCategoryIdScoreAsc(any(), anyInt());
-			Id categoryId = Id.generateNextId();
 			Id memberId = Id.generateNextId();
+			Id categoryId = Id.generateNextId();
+			Id otherMemberId = Id.generateNextId();
+			Category category = CategoryTestHelper.generateUnSharedCategory("title", memberId,
+				categoryId);
+			em.persist(category);
 
 			// expect
 			assertThatThrownBy(
-				() -> cardSimulationService.simulateScore(categoryId, memberId, 100)).isInstanceOf(
-				AccessDeniedException.class);
+				() -> cardSimulationService.simulateScore(categoryId, otherMemberId, 100))
+				.isInstanceOf(AccessDeniedException.class);
 		}
 
 		@Test
 		@DisplayName("카테고리에 속한 카드가 없는 경우 빈 리스트를 반환한다")
 		void shouldReturnEmptyListWhenCategoryHasNoCardsTest() {
 			// given
-			Mockito.doReturn(Optional.of(
-					CategoryTestHelper.generateSharedCategory("title", Id.generateNextId(), Id.generateNextId())))
-				.when(categoryRepository)
-				.findByCategoryIdAndIsDeleted(any(), anyBoolean());
-			Mockito.doReturn(List.of()).when(cardRepository).findByCategoryIdAndIsDeleted(any(), anyBoolean());
+			Id memberId = Id.generateNextId();
+			Id categoryId = Id.generateNextId();
+			Category category = CategoryTestHelper.generateUnSharedCategory("title", memberId,
+				categoryId);
+			em.persist(category);
 
 			// when
-			List<CardDto> result = cardSimulationService.simulateScore(Id.generateNextId(),
-				Id.generateNextId(), 100);
+			List<CardDto> result = cardSimulationService.simulateScore(categoryId, memberId, 100);
 
 			// then
 			assertThat(result).isEmpty();
@@ -227,19 +220,25 @@ class CardSimulationServiceTest {
 		@DisplayName("limit으로 제한한 카드 갯수로 출력한다")
 		void shouldReturnLimitedCardsTest() {
 			// given
-			Mockito.doReturn(Optional.of(
-					CategoryTestHelper.generateSharedCategory("title", Id.generateNextId(), Id.generateNextId())))
-				.when(categoryRepository)
-				.findByCategoryIdAndIsDeleted(any(), anyBoolean());
-			Mockito.doReturn(
-					List.of(CardTestHelper.genOxCard(Id.generateNextId(), Id.generateNextId(), Id.generateNextId()),
-						CardTestHelper.genOxCard(Id.generateNextId(), Id.generateNextId(), Id.generateNextId())))
-				.when(cardRepository).findCardByCategoryIdScoreAsc(any(), anyInt());
-			final int limit = 2;
+			Id memberId = Id.generateNextId();
+			Id categoryId = Id.generateNextId();
+			Id cardId1 = Id.generateNextId();
+			Id cardId2 = Id.generateNextId();
+			Id cardId3 = Id.generateNextId();
+			Category category = CategoryTestHelper.generateUnSharedCategory("title", memberId,
+				categoryId);
+			Card card1 = CardTestHelper.genOxCard(memberId, categoryId, cardId1);
+			Card card2 = CardTestHelper.genOxCard(memberId, categoryId, cardId2);
+			Card card3 = CardTestHelper.genOxCard(memberId, categoryId, cardId3);
+			em.persist(category);
+			em.persist(card1);
+			em.persist(card2);
+			em.persist(card3);
+			int limit = 2;
 
 			// when
-			List<CardDto> result = cardSimulationService.simulateScore(Id.generateNextId(),
-				Id.generateNextId(), limit);
+			List<CardDto> result = cardSimulationService.simulateScore(categoryId,
+				memberId, limit);
 
 			// then
 			assertThat(result).hasSize(limit);
@@ -254,14 +253,17 @@ class CardSimulationServiceTest {
 		@DisplayName("카테고리가 삭제되거나 없는 카테고리면 예외를 발생한다")
 		void shouldThrowIllegalArgumentExceptionWhenCategoryIsDeletedTest() {
 			// given
-			Mockito.doReturn(false)
-				.when(categoryRepository)
-				.existsByCategoryIdAndIsDeletedFalse(any());
-			Id randomId = Id.generateNextId();
+			Id memberId = Id.generateNextId();
+			Id categoryId = Id.generateNextId();
+			Category category = CategoryTestHelper.generateUnSharedCategory("title", memberId,
+				categoryId);
+			category.delete();
+			em.persist(category);
 
 			// expect
-			Assert.assertThrows(IllegalArgumentException.class,
-				() -> cardSimulationService.findCardCountByScore(randomId));
+			assertThatThrownBy(
+				() -> cardSimulationService.findCardCountByScore(categoryId))
+				.isInstanceOf(IllegalArgumentException.class);
 		}
 
 		@Test
