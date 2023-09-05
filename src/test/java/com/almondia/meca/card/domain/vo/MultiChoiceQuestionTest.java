47f21c250e2f9fd14d7c544b8ce79c6037d7790b
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-09-05 14:39:42 +0900

    fix: 객관식 질문 vo 잘못된 validation 수정
    세부 사항:
    - 정규식 대신 JSONArray의 파싱기능 활용

--- src/test/java/com/almondia/meca/card/domain/vo/MultiChoiceQuestionTest.java
+++ src/test/java/com/almondia/meca/card/domain/vo/MultiChoiceQuestionTest.java
@@ -1,63 +1,90 @@
 package com.almondia.meca.card.domain.vo;
 
-import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class MultiChoiceQuestionTest {
+	private static final int MAX_CHOICE_COUNT = 5;
+	private static final int MIN_CHOICE_COUNT = 2;
+	private static final int MAX_LENGTH_PER_VIEW_QUESTION = 100;
+	private static final int MAX_LENGTH = 51_000;
 
 	@Test
-	@DisplayName("퀴즈 문제에 비우거나 공백만 입력해서는 안됩니다")
-	void shouldNotBeBlankTest() {
-		String value = "";
-		assertThatThrownBy(() -> MultiChoiceQuestion.of(value))
+	@DisplayName("Question이 5_1000자를 초과하는 경우 예외 발생")
+	void shouldThrowExceptionWhenQuestionLengthIsOver5_1000Test() {
+		// given
+		String startPart = "[\"<p>";
+		String endPart = "</p>\",\"1\",\"2\"]";
+		int length = startPart.length() + endPart.length();
+		String inputString = startPart + "a".repeat(MAX_LENGTH - length + 1) + endPart;
+
+		// expect
+		assertThatThrownBy(() -> MultiChoiceQuestion.of(inputString))
 			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("퀴즈 문제에 비우거나 공백만 입력해서는 안됩니다");
+			.hasMessage("최대 " + MAX_LENGTH + "자까지 입력 가능합니다");
 	}
 
 	@Test
-	@DisplayName("퀴즈 문제는 [로 시작해서 ]로 끝나야 합니다")
-	void shouldStartEndSquareBracketTest() {
-		String value = "<p> hello, world </p>, \"hello, world\"";
-		assertThatThrownBy(() -> MultiChoiceQuestion.of(value))
+	@DisplayName("제대로된 jsonArray 형식이 아닌 경우 예외 발생")
+	void shouldThrowExceptionWhenInputIsNotJsonArrayTest() {
+		// given
+		String inputString = "not json array";
+
+		// expect
+		assertThatThrownBy(() -> MultiChoiceQuestion.of(inputString))
 			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("퀴즈 문제는 [로 시작해서 ]로 끝나야 합니다");
+			.hasMessage("JSON 형식이 아닙니다");
 	}
 
 	@Test
-	@DisplayName("퀴즈 문제의 선택지는 5개 이하로 입력해야 합니다")
-	void shouldNotMoreFiveSelectViewTest() {
-		String value = "[\\\"<p>DDOS 공격이 아닌것은? \"background-color:rgb(243,243,243);\"</p>\\\",\\\"Trin00\\\",\\\"TFN\\\",\\\"TFN2k\\\",\\\"Stacheldraht\\\",\\\"TFN3k\\\",\\\"TFN4k\\\"]";
-		assertThatThrownBy(() -> MultiChoiceQuestion.of(value))
+	@DisplayName("선택지가 2개 미만인 경우 예외 발생")
+	void shouldThrowExceptionWhenChoiceCountIsLessThan2Test() {
+		// given
+		String inputString = "[\"<p>question</p>\",\"1\"]";
+
+		// expect
+		assertThatThrownBy(() -> MultiChoiceQuestion.of(inputString))
 			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("퀴즈 문제의 선택지는 5개를 초과할 수 없습니다");
+			.hasMessage(String.format("선택지는 최소 %d개 이상이어야 합니다", MIN_CHOICE_COUNT));
 	}
 
 	@Test
-	@DisplayName("퀴즈 문제의 선택지는 1개 이상으로 입력해야 합니다")
-	void shouldMoreOneSelectViewTest() {
-		String value = "[\\\"<p>DDOS 공격이 아닌것은? \"background-color:rgb(243,243,243);\"</p>\\\"]";
-		assertThatThrownBy(() -> MultiChoiceQuestion.of(value))
+	@DisplayName("선택지가 5개 초과인 경우 예외 발생")
+	void shouldThrowExceptionWhenChoiceCountIsMoreThan5Test() {
+		// given
+		String inputString = "[\"<p>question</p>\",\"1\",\"2\",\"3\",\"4\",\"5\",\"6\"]";
+
+		// expect
+		assertThatThrownBy(() -> MultiChoiceQuestion.of(inputString))
 			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("퀴즈 문제의 선택지는 1개 이상이어야 합니다");
+			.hasMessage(String.format("선택지는 최대 %d개 이하여야 합니다", MAX_CHOICE_COUNT));
 	}
 
 	@Test
-	@DisplayName("퀴즈 문제의 선택지는 100자 이하로 입력해야 합니다")
-	void shouldNotMore100QuizStringLengthTest() {
-		String value = "[\\\"<p>DDOS 공격이 아닌것은?</p>\\\"" + ",\\\"" + "a".repeat(101) + "\\\"" + "]";
-		assertThatThrownBy(() -> MultiChoiceQuestion.of(value))
+	@DisplayName("보기 길이가 100자 초과인 경우 예외 발생")
+	void shouldThrowExceptionWhenViewLengthIsMoreThan100Test() {
+		// given
+		String lastView = "a".repeat(MAX_LENGTH_PER_VIEW_QUESTION + 1);
+		String inputString = String.format("[\"<p>question</p>\",\"1\",\"2\",\"%s\"]", lastView);
+
+		// expect
+		assertThatThrownBy(() -> MultiChoiceQuestion.of(inputString))
 			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("퀴즈 문제의 선택지는 100자를 초과할 수 없습니다");
+			.hasMessage(String.format("보기는 최대 %d자까지 입력 가능합니다", MAX_LENGTH_PER_VIEW_QUESTION));
 	}
 
 	@Test
-	@DisplayName("51,000자를 초과해서 입력하면 안된다")
-	void shouldNotMore51000Test() {
-		String value = "a".repeat(51001);
-		assertThatThrownBy(() -> MultiChoiceQuestion.of(value))
+	@DisplayName("보기가 비어있는 경우 예외 발생")
+	void shouldThrowExceptionWhenViewIsBlankTest() {
+		// given
+		String inputString = "[\"<p>question</p>\",\"1\",\"2\",\"\",\"4\"]";
+
+		// expect
+		assertThatThrownBy(() -> MultiChoiceQuestion.of(inputString))
 			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("51000를 초과해서 입력하셨습니다");
+			.hasMessage("보기는 비어 있을 수 없습니다");
 	}
+
 }
