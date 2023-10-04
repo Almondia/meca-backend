de4d260d813232c32e50b104b3d8d5ba1d14a745
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-10-04 19:56:33 +0900

    test: 카테고리 저장 테스트 추가

---
+++ src/test/java/com/almondia/meca/category/application/CategoryServiceTest2.java
@@ -0,0 +1,109 @@
+package com.almondia.meca.category.application;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.almondia.meca.card.domain.repository.CardRepository;
+import com.almondia.meca.cardhistory.domain.repository.CardHistoryRepository;
+import com.almondia.meca.category.application.helper.CategoryFactory;
+import com.almondia.meca.category.controller.dto.CategoryDto;
+import com.almondia.meca.category.controller.dto.SaveCategoryRequestDto;
+import com.almondia.meca.category.domain.entity.Category;
+import com.almondia.meca.category.domain.repository.CategoryRepository;
+import com.almondia.meca.category.domain.service.CategoryChecker;
+import com.almondia.meca.category.domain.vo.Title;
+import com.almondia.meca.common.domain.vo.Id;
+import com.almondia.meca.common.domain.vo.Image;
+import com.almondia.meca.member.domain.repository.MemberRepository;
+import com.almondia.meca.recommand.domain.repository.CategoryRecommendRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class CategoryServiceTest2 {
+
+	@InjectMocks
+	private CategoryService categoryService;
+
+	@Mock
+	private CategoryRepository categoryRepository;
+
+	@Mock
+	private CardRepository cardRepository;
+
+	@Mock
+	private CardHistoryRepository cardHistoryRepository;
+
+	@Mock
+	private CategoryChecker categoryChecker;
+
+	@Mock
+	private MemberRepository memberRepository;
+
+	@Mock
+	private CategoryRecommendRepository categoryRecommendRepository;
+
+	@Nested
+	@DisplayName("카테고리 등록 테스트")
+	class SaveCategoryTest {
+
+		@Test
+		@DisplayName("카테고리 등록시 카테고리가 정상적으로 등록을 요청하는지 확인한다")
+		void shouldCallRepositorySaveTest() {
+			//given
+			SaveCategoryRequestDto saveCategoryRequestDto = createSaveCategoryRequestDto();
+			Id memberId = Id.generateNextId();
+
+			when(categoryRepository.save(any(Category.class)))
+				.thenReturn(CategoryFactory.genCategory(saveCategoryRequestDto, memberId));
+
+			// when
+			CategoryDto categoryDto = categoryService.saveCategory(saveCategoryRequestDto, memberId);
+
+			// then
+			verify(categoryRepository, times(1)).save(any(Category.class));
+			assertThat(categoryDto.getTitle()).isEqualTo(saveCategoryRequestDto.getTitle());
+			assertThat(categoryDto.getMemberId()).isEqualTo(memberId);
+			assertThat(categoryDto.getThumbnail()).isEqualTo(saveCategoryRequestDto.getThumbnail());
+		}
+
+		@Test
+		@DisplayName("카테고리 등록시 썸네일이 없어도 정상적으로 등록을 요청하는지 테스트")
+		void shouldCallRepositorySaveTestExceptThumbnailTest() {
+			//given
+			SaveCategoryRequestDto saveCategoryRequestDto = createSaveCategoryRequestDtoExceptThumbnail();
+			Id memberId = Id.generateNextId();
+
+			when(categoryRepository.save(any(Category.class)))
+				.thenReturn(CategoryFactory.genCategory(saveCategoryRequestDto, memberId));
+
+			// when
+			CategoryDto categoryDto = categoryService.saveCategory(saveCategoryRequestDto, memberId);
+
+			// then
+			verify(categoryRepository, times(1)).save(any(Category.class));
+			assertThat(categoryDto.getTitle()).isEqualTo(saveCategoryRequestDto.getTitle());
+			assertThat(categoryDto.getMemberId()).isEqualTo(memberId);
+		}
+
+		private SaveCategoryRequestDto createSaveCategoryRequestDto() {
+			return SaveCategoryRequestDto.builder()
+				.title(Title.of("카테고리 제목"))
+				.thumbnail(Image.of("thumbnail"))
+				.build();
+		}
+
+		private SaveCategoryRequestDto createSaveCategoryRequestDtoExceptThumbnail() {
+			return SaveCategoryRequestDto.builder()
+				.title(Title.of("카테고리 제목"))
+				.build();
+		}
+	}
+}
