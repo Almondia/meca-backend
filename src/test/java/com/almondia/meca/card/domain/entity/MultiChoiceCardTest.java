e0656e870922ecfde80002ca2ad7ca5feaa43c45
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-10-10 21:35:36 +0900

    test: MultiChoiceCard 비어있는 커버리지 테스트

--- src/test/java/com/almondia/meca/card/domain/entity/MultiChoiceCardTest.java
+++ src/test/java/com/almondia/meca/card/domain/entity/MultiChoiceCardTest.java
@@ -12,6 +12,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import com.almondia.meca.card.domain.vo.MultiChoiceAnswer;
+import com.almondia.meca.card.domain.vo.MultiChoiceQuestion;
 import com.almondia.meca.common.configuration.jpa.JpaAuditingConfiguration;
 import com.almondia.meca.common.configuration.jpa.QueryDslConfiguration;
 
@@ -34,10 +36,35 @@ class MultiChoiceCardTest {
 		assertThat(entityType.getName()).isEqualTo("MultiChoiceCard");
 		assertThat(entityType.getAttributes()).extracting("name")
 			.containsExactlyInAnyOrder("description", "memberId", "question", "isDeleted", "cardId", "categoryId",
-				"title",
-				"images",
-				"createdAt",
-				"modifiedAt", "multiChoiceAnswer");
+				"title", "images", "createdAt", "modifiedAt", "multiChoiceAnswer");
+	}
+
+	@Test
+	@DisplayName("changeQuestion 테스트")
+	void changeQuestionTest() {
+		MultiChoiceCard multiChoiceCard = MultiChoiceCard.builder()
+			.question(MultiChoiceQuestion.of("[\"<p>question</p>\",\"1\",\"2\"]"))
+			.multiChoiceAnswer(MultiChoiceAnswer.valueOf("2"))
+			.build();
+		multiChoiceCard.changeQuestion("[\"<p>question</p>\",\"1\",\"2\",\"3\"]");
+		assertThat(multiChoiceCard.getQuestion()).isEqualTo("[\"<p>question</p>\",\"1\",\"2\",\"3\"]");
+	}
+
+	@Test
+	@DisplayName("changeAnswer 테스트")
+	void changeAnswerTest() {
+		MultiChoiceCard multiChoiceCard = MultiChoiceCard.builder().build();
+		multiChoiceCard.changeAnswer("2");
+		assertThat(multiChoiceCard.getAnswer()).isEqualTo("2");
+	}
+
+	@Test
+	@DisplayName("getAnswer 테스트")
+	void shouldReturnStringAnswerTest() {
+		MultiChoiceCard multiChoiceCard = MultiChoiceCard.builder()
+			.multiChoiceAnswer(MultiChoiceAnswer.valueOf("2"))
+			.build();
+		assertThat(multiChoiceCard.getAnswer()).isEqualTo("2");
 	}
 
 }
